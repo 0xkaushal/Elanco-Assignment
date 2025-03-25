b9704d1b4fd5508854685bb0f8c70c61c703f8a2
satvik kaushal <satvikkaushal8@gmail.com>
2025-03-25 16:53:32 +0530

    Update start script

--- Start.sh
+++ Start.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-
-# Install dependencies for frontend and backend
-echo "Installing dependencies for frontend and backend..."
-
-# Install concurrently if not already installed
 echo "Installing concurrently..."
 npm install concurrently
 
-# Install dependencies for frontend
+echo "Installing dependencies for frontend and backend..."
+
 cd country-data-frontend || exit
 npm install
 cd ..
 
-# Install dependencies for backend
 cd country-data-backend || exit
 npm install
 cd ..
 
-# Run both frontend and backend concurrently
 echo "Starting both frontend and backend..."
 
-# Run the frontend (Next.js) and backend (Express) concurrently
+# Run the frontend and backend concurrently
 npx concurrently "npm run build --prefix country-data-frontend && npm start --prefix country-data-frontend" "npm run dev --prefix country-data-backend"
